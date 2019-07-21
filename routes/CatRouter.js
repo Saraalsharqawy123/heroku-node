bfcf9132c8f4cf8a2053524bdc368178cf5d9063
Sara alsharqawy <saraalsharqawy123@gmail.com>
2019-07-21 20:34:16 +0300

    Added cors all

--- routes/CatRouter.js
+++ routes/CatRouter.js
@@ -22,6 +22,8 @@ var express = require('express');
    router.use((req, res, next)=>{
 
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+   
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
     
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -17,7 +17,9 @@ var publicKEY  = fs.readFileSync("./module/keys/p.key", 'utf8');
 
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
   next();
 });
 
